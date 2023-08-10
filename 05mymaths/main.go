3de14556010edef6a18dae2e186145c4d9dd3020
Taha <taha-chaoui@hotmail.com>
2023-08-10 18:33:56 +0100

    Finished

---
+++ 05mymaths/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"crypto/rand"
+	"fmt"
+	"math/big"
+)
+
+func main() {
+	fmt.Println("Welcome to maths in golang")
+
+	// var myNumberOne int = 2
+	// var myNumberTwo float64 = 4.5
+
+	// fmt.Println("The sum is: ", myNumberOne+int(myNumberTwo))
+
+	//random number
+	//rand.Seed(time.Now().UnixNano())
+	//fmt.Println(rand.Intn(10))
+
+	//random from crypto
+
+	myRandomNum, _ := rand.Int(rand.Reader, big.NewInt(5))
+	fmt.Println(myRandomNum)
+}
---
+++ 28channels/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	fmt.Println("Channels in golang- LearnCodeOnline.in")
+	wg := &sync.WaitGroup{}
+
+	myCh := make(chan int, 1)
+
+	// myCh <- 5
+	// fmt.Println(<-myCh)
+
+	wg.Add(2)
+	// R ONLY
+	go func(ch <-chan int, wg *sync.WaitGroup) {
+		val, isChannelOpen := <-myCh
+
+		fmt.Println(isChannelOpen)
+		fmt.Println(val)
+
+		//fmt.Println(<-myCh)
+
+		wg.Done()
+	}(myCh, wg)
+	// send ONLY
+	go func(ch <-chan int, wg *sync.WaitGroup) {
+		myCh <- 5
+		myCh <- 6
+		close(myCh)
+		wg.Done()
+	}(myCh, wg)
+
+	wg.Wait()
+}
